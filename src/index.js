0fdfe59f1a73347ec1d1dcc104fb9f51b356804c
ArturKljwa <kljwa1996@gmail.com>
2023-01-03 15:49:57 +0300

    feach: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
-}
+    let result = '';
+
+    let str  =  Math.abs(n) + '';
+
+    for (let i = str.length - 1; i >= 0 ; i--) {
+      result += str[i]
+    }
+    if (result[0] == 0) {
+      return result.substring(1)
+    }
+     else {
+      return result
+   }
+   }
+
